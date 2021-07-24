532cc54d4db77a929433e1ccd78d5d9046306c9d
Fernando Da Rós <fernando.ros@stone.com.br>
2021-07-24 19:32:46 -0300

    chore: add acc not found error

--- pkg/domain/usecases/accounts/erros.go
+++ pkg/domain/usecases/accounts/erros.go
@@ -3,6 +3,7 @@ package accounts
 import "errors"
 
 var (
+	ErrAccountNotFound     = errors.New("account not found")
 	ErrInvalidDocument     = errors.New("invalid document")
 	ErrInvalidAmount       = errors.New("invalid amount")
 	ErrInsufficientBalance = errors.New("insufficient balance")

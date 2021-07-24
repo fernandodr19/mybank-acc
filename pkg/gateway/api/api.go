7500730c569db550ff5a920dafe112efd535c5e6
Fernando Da Rós <fernando.ros@stone.com.br>
2021-07-24 19:59:45 -0300

    feat: build api handler

---
+++ pkg/gateway/api/api.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"net/http"
+
+	app "github.com/fernandodr19/mybank-acc/pkg"
+	"github.com/fernandodr19/mybank-acc/pkg/config"
+	"github.com/fernandodr19/mybank-acc/pkg/gateway/api/accounts"
+	"github.com/fernandodr19/mybank-acc/pkg/gateway/api/middleware"
+
+	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	http_swagger "github.com/swaggo/http-swagger"
+	"github.com/urfave/negroni"
+)
+
+// BuildHandler builds api handler
+func BuildHandler(app *app.App, cfg *config.Config) (http.Handler, error) {
+	r := mux.NewRouter()
+
+	r.PathPrefix("/metrics").Handler(promhttp.Handler()).Methods(http.MethodGet)
+	r.PathPrefix("/healthcheck").HandlerFunc(func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusOK) }).Methods(http.MethodGet)
+	r.PathPrefix("/docs/v1/mybank/transactions/swagger").Handler(http_swagger.WrapHandler).Methods(http.MethodGet)
+
+	publicV1 := r.PathPrefix("/api/v1").Subrouter()
+	adminV1 := r.PathPrefix("/admin/v1").Subrouter()
+	accounts.NewHandler(publicV1, adminV1, *app.Accounts)
+
+	recovery := negroni.NewRecovery()
+	recovery.PrintStack = false
+	n := negroni.New()
+	n.UseFunc(middleware.TrimSlashSuffix)
+	n.UseFunc(middleware.AssureRequestID)
+	n.UseHandler(middleware.Cors(r))
+
+	return n, nil
+}

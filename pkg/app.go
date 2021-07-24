190ef5ee458b7d266efc3fc2af4078247a879fb9
Fernando Da Rós <fernando.ros@stone.com.br>
2021-07-24 20:00:10 -0300

    feat: build app

---
+++ pkg/app.go
@@ -0,0 +1,21 @@
+package app
+
+import (
+	"github.com/fernandodr19/mybank-acc/pkg/domain/usecases/accounts"
+	"github.com/fernandodr19/mybank-acc/pkg/gateway/db/postgres"
+
+	"github.com/jackc/pgx/v4"
+)
+
+// App contains application's usecases
+type App struct {
+	Accounts *accounts.Usecase
+}
+
+// BuildApp builds application struct with its necessary usecases
+func BuildApp(dbConn *pgx.Conn) (*App, error) {
+	accRepo := postgres.NewAccountsRepository(dbConn)
+	return &App{
+		Accounts: accounts.NewUsecase(accRepo),
+	}, nil
+}
